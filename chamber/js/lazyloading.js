cd5cc0412068196eb65ec2cde86f81a6bd9bc609
Brandi-Galvan <glo19001@byui.edu>
2022-02-20 17:01:20 -0700

    fix lazy load

--- chamber/js/lazyloading.js
+++ chamber/js/lazyloading.js
@@ -1,25 +1,34 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+const allImages = document.querySelectorAll("img[data-src]")
+
+const lazyLoad = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"))
+    img.onload = () => {
+        img.removeAttribute("data-src")
+        img.className = "in"
+    };
 };
 
-if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+if ('IntersectionObserver' in window) {
+    const obsrvr = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                lazyLoad(item.target)
+                observer.unobserve(item.target)
+            }
+        })
+    }, options)
+    allImages.forEach((img) => {
+        obsrvr.observe(img)
+    })
+}
+else {
+    allImages.forEach((img) => {
+        lazyLoad(img)
+    })
 }
