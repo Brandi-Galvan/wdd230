1a11da944108d4095c4b2dab827ce9a93a3f564a
Brandi-Galvan <glo19001@byui.edu>
2022-03-11 23:54:28 -0700

    blah

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,27 @@
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=gilbert&units=imperial&appid=157dea0b1a28c8415ca1013f1e78acd2'
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const t = jsObject.main.temp.toFixed(1);
+    document.querySelector('#current-temp').textContent = t;
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    const windsp = jsObject.wind.speed;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('#desc').textContent = desc;
+    document.querySelector('#speed').textContent = windsp;
+
+
+    if(t <= 50 && windsp > 3) {
+      const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(windsp,0.16) + 0.4275 * t * Math.pow(windsp,0.16)
+      document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+    }
+    else {
+        document.querySelector("#windchill").innerHTML = "N/A"
+    }
+  });
