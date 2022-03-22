63c5459086de39cccbfe2ad58e105c7641c48b1f
Brandi-Galvan <glo19001@byui.edu>
2022-03-21 19:56:18 -0700

    spotlight

---
+++ chamber/js/spotlight.js
@@ -0,0 +1,41 @@
+const requestURL = "https://brandi-galvan.github.io/wdd230/chamber/data/data.json"
+const spotDiv = document.querySelectorAll(".spotDiv")
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const members = jsonObject['members'];
+        const filteredMembers = members.filter((member) => {
+            return member.membership == "Bronze" || member.membership == "Silver" || business.membership == "Gold";
+        });
+        spotDiv.forEach((spot,index) => {
+            const i = Math.floor(Math.random()*filteredMembers.length);
+            const member = filteredMembers[i];
+
+            //logo for spotlight
+            let image = document.createElement('img');
+            image.src = member.logo;
+            image.setAttribute('alt', member.name);
+            spot.appendChild(image);
+
+            //p phone for spotlight
+            let p2 = document.createElement('p');
+            p2.textContent = member.phone;
+            spot.appendChild(p2);
+
+            //p website for spotlight
+            let p3 = document.createElement('p');
+            p3.textContent = member.website;
+            spot.appendChild(p3);
+
+
+            filteredMembers.splice(i,1);
+        });
+    });
+
+
+
+
+
