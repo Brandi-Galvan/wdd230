288ec902829509a603d92e0361c5288fcf3d799a
Brandi-Galvan <glo19001@byui.edu>
2022-04-03 11:48:26 -0700

    home style

--- Temple/js/myscripts.js
+++ Temple/js/myscripts.js
@@ -3,16 +3,21 @@ let text = document.lastModified;
 document.getElementById("footerDate").innerHTML = text;
 
 //Hamburger menu
-const nav = document.querySelector(".navigation");
-const hamButton = document.querySelector(".hamburger");
 
-hamButton.addEventListener('click', () => {
-    nav.classList.toggle('responsive')
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation');
+const exitbutton = document.querySelector('#xbutton');
+const ham = document.querySelector('#ham');
+
+menubutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+    exitbutton.classList.toggle('hidden');
+    ham.classList.toggle('hidden');
 }, false);
 
 // Solving resizing issue
 window.onresize = () => {
-    if (window.innerWidth > 760) {
+    if (window.innerWidth > 768) {
         mainnav.classList.remove('responsive');
         exitbutton.classList.add("hidden");
         ham.classList.remove("hidden");
