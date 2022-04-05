b71962717a649bbe6a98a87a5064f2537f452787
Brandi-Galvan <glo19001@byui.edu>
2022-04-04 21:33:05 -0700

    lu

--- Temple/js/temples.js
+++ Temple/js/temples.js
@@ -58,3 +58,16 @@ fetch(requestURL)
 
         cards.prepend(card);
     }
+
+//temple hover menu
+
+const templeheader = document.querySelector('.temple-cards');
+const card = document.querySelector('.navigation');
+const exitbutton = document.querySelector('#xbutton');
+const ham = document.querySelector('#ham');
+
+menubutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+    exitbutton.classList.toggle('hidden');
+    ham.classList.toggle('hidden');
+}, false);
